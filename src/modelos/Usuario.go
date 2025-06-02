5d89c86442d1d2f25a1f4964b297822dbe451248
Francisco <franciscoocontatoo@gmail.com>
2025-06-02 20:46:19 -0300

    Implementando o método badoux para verificação do email

--- src/modelos/Usuario.go
+++ src/modelos/Usuario.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"strings"
 	"time"
+
+	"github.com/badoux/checkmail"
 )
 
 // Usuário representa um usuário utilizando a rede social
@@ -37,6 +39,10 @@ func (usuario *Usuario) validar(etapa string) error {
 
 	if usuario.Email == "" {
 		return errors.New("O email é obrigatório e não pode está em branco")
+	}
+
+	if erro := checkmail.ValidateFormat(usuario.Email); erro != nil {
+		return errors.New("O e-mail inserido é invalido")
 	}
 
 	if etapa == "cadastro" && usuario.Senha == "" {
