a29cedc8fb25e933ea40361f34bde42a3cc4fde1
Francisco <franciscoocontatoo@gmail.com>
2025-06-18 01:04:42 -0300

    Criando a entidade publicações na API e no DB

---
+++ src/modelos/Publicacao.go
@@ -0,0 +1,14 @@
+package modelos
+
+import "time"
+
+// Publicação representa uma publicação feita por um usuário
+type Publicacao struct {
+	ID        uint64    `json:"id,omitempy"`
+	Titulo    string    `json:"titulo,omitempy"`
+	Conteudo  string    `json:"conteudo,omitempy"`
+	AutorID   string    `json:"autorId,omitempy"`
+	AutorNick string    `json:"autorNick,omitempy"`
+	Curtidas  uint64    `json:"curtidas"`
+	CriadaEm  time.Time `json:"criadaEm,omitempy"`
+}
