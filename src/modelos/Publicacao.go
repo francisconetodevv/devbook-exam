6f1c434cb82037b6ca33a3d2e948d719e0ae64df
Francisco <franciscoocontatoo@gmail.com>
2025-06-18 20:47:46 -0300

    Enviando a criação do CRUD - Create publicacoes

--- src/modelos/Publicacao.go
+++ src/modelos/Publicacao.go
@@ -7,7 +7,7 @@ type Publicacao struct {
 	ID        uint64    `json:"id,omitempy"`
 	Titulo    string    `json:"titulo,omitempy"`
 	Conteudo  string    `json:"conteudo,omitempy"`
-	AutorID   string    `json:"autorId,omitempy"`
+	AutorID   uint64    `json:"autorId,omitempy"`
 	AutorNick string    `json:"autorNick,omitempy"`
 	Curtidas  uint64    `json:"curtidas"`
 	CriadaEm  time.Time `json:"criadaEm,omitempy"`
