f6a26a1a5e6fbf52297af6ab3bf62bde8daf3369
Francisco <franciscoocontatoo@gmail.com>
2025-06-18 21:01:54 -0300

    Enviando o ajuste com a validação dos campos, se está preenchido ou nao

--- src/modelos/Publicacao.go
+++ src/modelos/Publicacao.go
@@ -1,6 +1,10 @@
 package modelos
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 // Publicação representa uma publicação feita por um usuário
 type Publicacao struct {
@@ -11,4 +15,30 @@ type Publicacao struct {
 	AutorNick string    `json:"autorNick,omitempy"`
 	Curtidas  uint64    `json:"curtidas"`
 	CriadaEm  time.Time `json:"criadaEm,omitempy"`
+}
+
+func (publicacao *Publicacao) Preparar() error {
+	if erro := publicacao.validar(); erro != nil {
+		return erro
+	}
+
+	publicacao.formatar()
+	return nil
+}
+
+func (publicacao *Publicacao) validar() error {
+	if publicacao.Titulo == "" {
+		return errors.New("O título é obrigatório e não pode está em branco!")
+	}
+
+	if publicacao.Conteudo == "" {
+		return errors.New("O conteudo é obrigatório e não pode estar em branco!")
+	}
+
+	return nil
+}
+
+func (publicacao *Publicacao) formatar() {
+	publicacao.Titulo = strings.TrimSpace(publicacao.Titulo)
+	publicacao.Conteudo = strings.TrimSpace(publicacao.Conteudo)
 }
